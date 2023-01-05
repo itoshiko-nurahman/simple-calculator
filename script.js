acfc6bbf5478a868a19f65b0735da6d8f7367b76
Itoshiko Nurahman <itoshikonurahman@gmail.com>
2023-01-06 05:10:15 +0700

    Make delete button function

--- script.js
+++ script.js
@@ -4,24 +4,33 @@ let lastDisplay = document.getElementById("last-display");
 let numbers = document.querySelectorAll(".number");
 // Get number value when the button is clicked
 numbers.forEach(number => {
-  number.addEventListener('click', event => {
-    let numberValue = event.target.value;
-    currentDisplay.innerHTML += numberValue;
+  number.addEventListener('click', (event) => {
+    numInput(event);
   });
 });
+
+function numInput (number) {
+  if (currentDisplay.textContent == 0) {
+    let numberValue = number.target.textContent;
+    currentDisplay.innerHTML = numberValue;
+  } else {
+    let numberValue = number.target.textContent;
+    currentDisplay.innerHTML += numberValue;
+  }
+}
 
 // clear the screen
 let clear = document.getElementById('btn-clear');
 clear.addEventListener('click', () => {
-  currentDisplay.innerHTML = '';
+  currentDisplay.innerHTML = 0;
   lastDisplay.innerHTML = '';
 });
 
 // delete 1 number, don't know how to do it
-// let btnDelete = document.getElementById('btn-delete');
-// btnDelete.addEventListener('click', () => {
-//   currentDisplay.innerHTML -= 1;
-// });
+let btnDelete = document.getElementById('btn-delete');
+btnDelete.addEventListener('click', () => {
+  currentDisplay.textContent = currentDisplay.textContent.toString().slice(0, -1);
+});
 
 // add the number
 let operations = document.querySelectorAll('.operator')
@@ -29,5 +38,10 @@ operations.forEach(operation => {
   operation.addEventListener('click', () => {
     let NumOpe = operation.textContent;
     lastDisplay.innerHTML = `${currentDisplay.textContent} ${NumOpe}`;
+    currentDisplay.innerHTML = '';
   })
 })
+
+function NumOperation () {
+
+}
