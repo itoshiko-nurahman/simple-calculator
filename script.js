55b3aede165cb052c786871a2fdde941be4dd21b
Itoshiko Nurahman <itoshikonurahman@gmail.com>
2023-01-05 10:11:45 +0700

    Make Operation button works

--- script.js
+++ script.js
@@ -10,9 +10,24 @@ numbers.forEach(number => {
   });
 });
 
+// clear the screen
 let clear = document.getElementById('btn-clear');
 clear.addEventListener('click', () => {
   currentDisplay.innerHTML = '';
   lastDisplay.innerHTML = '';
 });
 
+// delete 1 number, don't know how to do it
+// let btnDelete = document.getElementById('btn-delete');
+// btnDelete.addEventListener('click', () => {
+//   currentDisplay.innerHTML -= 1;
+// });
+
+// add the number
+let operations = document.querySelectorAll('.operator')
+operations.forEach(operation => {
+  operation.addEventListener('click', () => {
+    let NumOpe = operation.textContent;
+    lastDisplay.innerHTML = `${currentDisplay.textContent} ${NumOpe}`;
+  })
+})
